19526ae0855c00c06abdead1478724d8367f0119
Arturo0911 <anegreiross@outlook.com>
2022-11-02 20:50:50 -0500

    ok

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 
 
-//const data = require('data');
 
-const log = () =>{
-  console.log("hello world");
+function addition(num1, num2){
+  return num1 + num2;
 }
 
+function calc(num1, num2, callback){
+  return callback(num1, num2);
+}
 
-log()
+console.log(calc(15, 16, addition));
+
+setTimeout(function(){
+  console.log("hello motherfuckers")
+}, 2000)
